a4b47321a549007b332a91872a626927dd6f9ab5
firatkocoglu <fratkocogl@gmail.com>
2024-04-23 21:29:30 +0300

    Add Category, UAV, Rental models. Add serializers for related models.

--- rental/models.py
+++ rental/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils.timezone import now
 
-# Create your models here.
+#DEFINE CATEGORIES FOR UAV'S
+class Category(models.Model):
+    category = models.CharField(max_length=255, db_index=True)
+    
+    def __str__(self):
+        return self.category
+
+#DEFINE UAV FEATURES
+class UAV(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.PROTECT)
+    brand = models.CharField(max_length=255)
+    model = models.CharField(max_length=255)
+    weight = models.IntegerField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    
+    def __str__(self):
+        return self.brand + ' ' + self.title
+    
+
+#DEFINE UAV RENTAL RECORD DETAILS
+class Rental(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    uav = models.ForeignKey(UAV, on_delete=models.PROTECT)
+    date = models.DateTimeField(default=now)
+    total = models.DecimalField(max_digits=8, decimal_places=2)
+    
+    def __str__(self):
+        return self.user + ' ' + self.uav
---
+++ rental/serializers.py
@@ -0,0 +1,33 @@
+from rest_framework import serializers
+from .models import Category, UAV, Rental
+from django.contrib.auth.models import User
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'first_name', 'last_name', 'email']
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = ['id', 'category']
+        
+class UAVSerializer(serializers.ModelSerializer):   
+    #USERS SHOULD ONLY TYPE CATEGORY ID TO LINK AN UAV TO A CATEGORY
+    category = CategorySerializer(read_only=True)
+    category_id = serializers.IntegerField(write_only=True)
+    
+    class Meta:
+        model = UAV
+        fields = ['id', 'category_id', 'category', 'brand', 'model', 'weight', 'price']
+
+class RentalSerializer(serializers.ModelSerializer):
+    user = UserSerializer(read_only=True)
+    user_id = serializers.IntegerField(write_only=True)
+    
+    uav = UAVSerializer(read_only=True)
+    uav_id = serializers.IntegerField(write_only=True)
+    
+    class Meta:
+        model = Rental
+        fields = ['id', 'user_id', 'user', 'uav_id', 'uav', 'date', 'total']
